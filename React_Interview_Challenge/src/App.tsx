ec74726997753a282758d92c5cc648b28d4a4dae
tysonOliveira <thaisonoliveira@gmail.com>
2023-02-08 01:47:32 -0300

    feat: adds undo and redo function

--- React_Interview_Challenge/src/App.tsx
+++ React_Interview_Challenge/src/App.tsx
@@ -3,6 +3,7 @@ import './app.css';
 
 export function App() {
   const [list, setList] = useState<{ clientX: number, clientY: number }[]>([]);
+  const [undid, setUndid] = useState<{ clientX: number, clientY: number }[]>([]);
 
   const handleClick = (event: React.MouseEvent) => {
     const newDot = {
@@ -18,11 +19,39 @@ export function App() {
     // impede que seja criado um circulo em cima do botão ao clicarmos nele
     event.stopPropagation();
     console.log('undo');
+
+    if (list.length === 0) {
+      return;
+    }
+
+    const lastItem = list[list.length - 1];
+    setUndid((prev) => [...prev, lastItem]);
+
+    setList((prev) => {
+      const newArr = [...prev].slice(0, -1);
+      return newArr;
+    });
+  }
+
+  const handleRedo = (event: React.MouseEvent) => {
+    event.stopPropagation();
+
+    if (undid.length === 0) {
+      return;
+    }
+
+    const recoveredDot = undid[undid.length - 1];
+    setUndid((prev) => {
+      const newArr = [...prev].slice(0, -1);
+      return newArr;
+    });
+    setList((prev) => [...prev, recoveredDot]);
   }
 
   return (
     <div className='app' onClick={handleClick}>
       <button onClick={handleUndo} >Desfazer</button>
+      <button onClick={handleRedo} >Refazer</button>
       {list.map((item) =>
         <span
           className='dot'
