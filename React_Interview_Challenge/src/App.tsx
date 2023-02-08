3bb736ba4e408f1571fface80e3a3e418f614048
tysonOliveira <thaisonoliveira@gmail.com>
2023-02-08 01:12:33 -0300

    feat: added handleClick function

--- React_Interview_Challenge/src/App.tsx
+++ React_Interview_Challenge/src/App.tsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { MouseEventHandler, useState } from 'react';
+import './app.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App() {
+  const [list, setList] = useState<{ clientX: number, clientY: number }[]>([]);
+
+  const handleClick = (event: React.MouseEvent) => {
+    const newDot = {
+      clientX: event.clientX,
+      clientY: event.clientY,
+    };
+
+    console.log(newDot)
+    setList((prev) => [...prev, newDot]);
+  }
+
+  const handleUndo = (event: React.MouseEvent) => {
+    // impede que seja criado um circulo em cima do botão ao clicarmos nele
+    event.stopPropagation();
+    console.log('undo');
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <div className='app' onClick={handleClick}>
+      <button onClick={handleUndo} >Desfazer</button>
+      {list.map((item) =>
+        <span
+          className='dot'
+          style={{ left: `${item.clientX - 25}px`, top: `${item.clientY - 25}px` }} />)}
+    </div >
   )
 }
-
-export default App
